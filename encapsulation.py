c2d4501b4178cb62635e884792e3afaa44785a75
Alejandrozic <alejandrozic@gmail.com>
2018-10-28 00:03:41 +0100

    first commit

---
+++ encapsulation.py
@@ -0,0 +1,25 @@
+
+"""
+Notes:
+    Implementation using @property
+"""
+
+class MyClass(object):
+
+    def __init__(self,value):
+        self._value = value
+
+    @property
+    def var(self):
+        """ Get attribute """
+        return self._value
+
+    @var.setter
+    def var(self,value):
+        """ Set attribute """
+        self._value = value
+
+    @var.deleter
+    def var(self):
+        """ Delete attribute """
+        self._value = None

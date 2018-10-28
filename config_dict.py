a3d950bb225b5c6d706e040e1edf1daef53e366a
Alejandrozic <alejandrozic@gmail.com>
2018-10-28 01:07:26 +0100

    edit error description

--- config_dict.py
+++ config_dict.py
@@ -15,7 +15,7 @@ class ConfigDict(dict):
     def __init__(self,filename):
         self._filename = filename
         try:
-            assert os.path.isfile(self._filename),"File not found"
+            assert os.path.isfile(self._filename),"ConfigDict:: File {0} not found".format(self._filename)
             with open(filename) as fh:
                 for line in fh:
                     line = line.rstrip()

bd3e6f60275cba428beee1826c9e83eb0a225cd1
AZ <alejandrozic@gmail.com>
2018-11-03 21:10:23 -0400

    comments and list w/commas

--- config_dict.py
+++ config_dict.py
@@ -1,31 +1,31 @@
 """
 :return: dict
-    format {key}={val}
-        sql_query=SELECT a FROM b WHERE condition
-        email_to=dl@domain.com
-        debug=True
-    read config as dictionary from file
-    write new edited config dict to file
+    Format key=val or key=v1,v2,v3,...
+    Read config file to dict
 """
 
 import os
 
+
 class ConfigDict(dict):
 
-    def __init__(self,filename):
+    def __init__(self, filename):
         self._filename = filename
         try:
-            assert os.path.isfile(self._filename),"ConfigDict:: File {0} not found".format(self._filename)
-            with open(filename) as fh:
+            assert os.path.isfile(self._filename), "ConfigDict:: File {0} not found".format(self._filename)
+            with open(self._filename) as fh:
                 for line in fh:
                     line = line.rstrip()
                     key, value = line.split('=', 1)
+                    if len(value.split(',')) > 1:
+                        value = value.split(',')
                     dict.__setitem__(self, key, value)
+
         except AssertionError as error:
             print error
 
     def __setitem__(self, key, value):
-        dict.__setitem__(self,key,value)
-        with open(self._filename,"w") as fh:
-            for key,val in self.items():
-                fh.write('{0}={1}'.format(key,val))
+        dict.__setitem__(self, key, value)
+        with open(self._filename, "w") as fh:
+            for key, val in self.items():
+                fh.write('{0}={1}'.format(key, val))

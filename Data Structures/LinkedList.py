ca27b1197af78f481969d9299924aace763b03ef
Alejandrozic <alejandrozic@gmail.com>
2018-11-20 20:07:22 +0000

    init add

---
+++ Data Structures/LinkedList.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+
+
+class LinkedList:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def append(self, data): pass
+
+    def prepend(self, data): pass
+
+    def delete(self, data): pass
+
+    def get_nodes(self): pass
+
+    def get_count_elements(self): pass
---
+++ Data Structures/MyLinkedList.py
@@ -0,0 +1,110 @@
+from LinkedList import LinkedList
+
+
+class MySinglyLinkedList(LinkedList):
+
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = node
+
+    def prepend(self, data):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+            return
+        node.next = self.head
+        self.head = node
+
+    def delete(self, data):
+        last = self.head
+
+        # Empty head
+        if not last:
+            return
+
+        # Data located in head
+        if last:
+            if last.data == data:
+                self.head = last.next
+                return
+
+        # Search for data
+        prev = None
+        while last:
+            if last.data == data:
+                prev.next = last.next
+                break
+            prev = last
+            last = last.next
+
+    def get_nodes(self):
+        node_data = []
+        last = self.head
+        while last:
+            node_data.append(last.data)
+            last = last.next
+        return node_data
+
+    def get_count_elements(self):
+        count = 0
+        last = self.head
+        while last:
+            count += 1
+            last = last.next
+        return count
+
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+if __name__ == '__main__':
+    ###########
+    # Testing #
+    ###########
+
+    linkedlist = MySinglyLinkedList()
+
+    # Append
+    linkedlist.append('b')
+    linkedlist.append('c')
+    linkedlist.append('d')
+    linkedlist.append('e')
+    linkedlist.append('f')
+    linkedlist.append('g')
+    linkedlist.append('h')
+
+    # Prepend
+    linkedlist.prepend('a')
+
+    # Delete - item not on list
+    linkedlist.delete('not_here')
+
+    # Delete - head
+    linkedlist.delete('a')
+
+    # Delete - middle
+    linkedlist.delete('e')
+
+    # Delete - last
+    linkedlist.delete('h')
+
+    # Get of each node element
+    # output: ['b', 'c', 'd', 'f', 'g']
+    print linkedlist.get_nodes()
+
+    # Count of Node Elements
+    # output: 5
+    print linkedlist.get_count_elements()

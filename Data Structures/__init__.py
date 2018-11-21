29f7402c063a96ca671c33f3259930f62e407dc6
Alejandrozic <alejandrozic@gmail.com>
2018-11-21 13:46:50 +0000

    added notes

---
+++ Data Structures/__init__.py
@@ -0,0 +1,15 @@
+"""
+Arrays
+    Search          O(1)
+    Deletion
+        -Start      O(N)
+        -End        O(1)
+    Memory Waste    0 (zero)
+
+LinkedList
+    Search          O(N)
+    Deletion
+        -Start      O(1)
+        -End        O(N)
+    Memory Waste    O(N)
+"""

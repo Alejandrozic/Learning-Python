8c6af03ecd51e2b4edc9a3c267565404c28acc99
Alejandrozic <alejandrozic@gmail.com>
2019-10-18 13:39:29 -0400

    new kata attempt

--- codewars.py
+++ codewars.py
@@ -5,3 +5,11 @@
 # -- KATA Disemvowel Trolls 7kyu
 def disemvowel(string):
     return ''.join([c for c in string if c.lower() not in {'a','e','i','o','u'}])
+
+# -- KATA Categorize New Member 7kyu
+def openOrSenior(data):
+    output = list()
+    for person in data:
+        age, handicap = person[0], person[1]
+        output.append('Senior' if age > 54 and handicap > 7 else 'Open')
+    return output

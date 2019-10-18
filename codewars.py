538c07f4b410d71dbcff9d206f9e4ea370c3d838
Alejandrozic <alejandrozic@gmail.com>
2019-10-18 16:48:33 -0400

    Update codewars.py

--- codewars.py
+++ codewars.py
@@ -52,3 +52,31 @@ def find_missing_letter(chars):
                alphabet.index(chars[-1])+1
                ]
     return list(set(alphabet) - set(chars))[0]
+
+"""
+KATA Bouncing Balls 6kyu
+
+A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+His mother looks out of a window 1.5 meters from the ground.
+How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+Three conditions must be met for a valid experiment:
+Float parameter "h" in meters must be greater than 0
+Float parameter "bounce" must be greater than 0 and less than 1
+Float parameter "window" must be less than h.
+If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+"""
+
+def bouncingBall(h, bounce, window):
+    if 0 < bounce < 1 and 0 < h < window:
+        return -1
+    count = 0
+    while h >= window:
+        # Fall
+        count += 1
+        # Contact with Floor
+        h = h * bounce
+        # Rise
+        if h >= window:
+            count += 1
+    return count

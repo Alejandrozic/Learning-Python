0ca0595b0a9134f38c750c897df520b322cdcbd5
Alejandrozic <alejandrozic@gmail.com>
2019-10-18 14:44:14 -0400

    Update codewars.py

--- codewars.py
+++ codewars.py
@@ -1,5 +1,7 @@
 """
-    My solutions to CodeWar challenges
+    ###################################
+     My solutions to CodeWar challenges
+    ###################################
 """
 
 # -- KATA Disemvowel Trolls 7kyu
@@ -18,3 +20,16 @@ def openOrSenior(data):
 def find_next_square(sq):
     n = sq**0.5
     return -1 if n % 1 else (n+1) **2
+
+"""
+KATA Ones and Zeros 7kyu
+
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+"""
+def binary_array_to_number(arr):
+    sum_ = 0
+    while arr:
+        binary, exponent = arr.pop(0), len(arr)
+        sum_ += binary * (2 ** exponent)
+    return sum_
